2c5d8204609dd424ecc03e5d0c7d68cc5cdabe32
Melony Franchini <meltravelz@gmail.com>
2023-02-02 13:40:29 -0700

    Add part1 test for user story 6

--- spec/features/exhibits/index_spec.rb
+++ spec/features/exhibits/index_spec.rb
@@ -17,18 +17,27 @@ RSpec.describe 'the exhibit index page' do
 
   describe 'user story 6' do
     describe 'when I visit "/exhibits"'do 
-      it 'displays exhibits ordered by most recently created first & when it was created' do
-        exhibit_1 = Exhibit.create!(name: "Ancient Rome", start_date: "2022-11-15", end_date: "2023-02-14", on_display: true, price: 15.00)
-        exhibit_2 = Exhibit.create!(name: "Ancient Korea", start_date: "2023-04-14", end_date: "2023-06-20", on_display: false, price: 17.00)
+      it 'displays exhibits ordered by most recently created first' do
+        exhibit_1 = Exhibit.create!(name: "Ancient Rome", start_date: "2022-11-15", end_date: "2023-02-14", on_display: true, price: 15.00, created_at: Time.now - 2.hour)
+        # this will also change the created_by by waiting a sec before it's created
+        # sleep(1)
+        exhibit_2 = Exhibit.create!(name: "Ancient Korea", start_date: "2023-04-14", end_date: "2023-06-20", on_display: false, price: 17.00, created_at: Time.now - 1.hour)
 
         visit "/exhibits"
 
-        expect(page).to have_content()
-        
+        expect(exhibit_2.name).to appear_before(exhibit_1.name)
+      end
+
+      it 'displays next to each of the records I see when it was created' do
+        exhibit_1 = Exhibit.create!(name: "Ancient Rome", start_date: "2022-11-15", end_date: "2023-02-14", on_display: true, price: 15.00, created_at: Time.now - 1.hour)
+        exhibit_2 = Exhibit.create!(name: "Ancient Korea", start_date: "2023-04-14", end_date: "2023-06-20", on_display: false, price: 17.00, created_at: Time.now - 2.hour)
+
+        visit "/exhibits"
+
         expect(page).to have_content("Created at: #{exhibit_1.created_at}")
         expect(page).to have_content("Created at: #{exhibit_2.created_at}")
-
       end
     end
   end
+
 end
