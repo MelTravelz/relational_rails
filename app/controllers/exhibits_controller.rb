7806586229450491e28945c1e3c61cc8daa70bdc
Melony Franchini <meltravelz@gmail.com>
2023-02-02 17:21:37 -0700

    Fix user story 6/moved logic from exhibit controller to exhibit model

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -1,7 +1,9 @@
 class ExhibitsController < ApplicationController
 
   def index
-    @exhibits = Exhibit.order(created_at: :desc)
+    @exhibits = Exhibit.order_by_most_recently_created
+    #If used this line the class method wouldn't be necessary: order_by_most_recently_created 
+    # @exhibits = Exhibit.order(created_at: :desc)
   end
 
   def show
--- spec/models/exhibit_spec.rb
+++ spec/models/exhibit_spec.rb
@@ -1,20 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Exhibit, type: :model do
+  before do
+    Artifact.destroy_all
+    Exhibit.destroy_all
+  end 
+
   it {should have_many :artifacts}
 
   describe 'instance methods' do
-    describe 'user story 7' do
-      describe '#count_of_artifacts' do
-        it 'returns the count of artifacts in an exhibit' do
-          exhibit_1 = Exhibit.create!(name: "Ancient Rome", start_date: "2022-11-15", end_date: "2023-02-14", on_display: true, price: 15.00)
+    describe 'user story 6' do
+      describe '#order_by_most_recently_created' do
+        it 'displays the exhibits in order of most recetly created first' do
+          exhibit_1 = Exhibit.create!(name: "Ancient Rome", start_date: "2022-11-15", end_date: "2023-02-14", on_display: true, price: 15.00, created_at: Time.now - 2.hour)
+          exhibit_2 = Exhibit.create!(name: "Ancient Korea", start_date: "2023-04-14", end_date: "2023-06-20", on_display: false, price: 17.00, created_at: Time.now - 1.hour)
 
-          artifact_1 = Artifact.create!(exhibit: exhibit_1, name: "Statue of Augustus", material: "marble", year_created: "45 BCE", on_loan: false, total_pieces: 5) 
-          artifact_2 = Artifact.create!(exhibit: exhibit_1, name: "Nummus Aureus Coin", material: "gold", year_created: "312 CE", on_loan: true, total_pieces: 12) 
-          artifact_3 = Artifact.create!(exhibit: exhibit_1, name: "Galdiator Cup", material: "glass", year_created: "75 BCE", on_loan: true, total_pieces: 1) 
-
-          expect(exhibit_1.count_of_artifacts).to eq(3)
+          # This expect is calling on all the Exhibit records... a class method: 
+          expect(Exhibit.order_by_most_recently_created.to_a).to eq([exhibit_2, exhibit_1])
         end
+      end
+    end
+  end
+
+  describe 'user story 7' do
+    describe '#count_of_artifacts' do
+      it 'returns the count of artifacts in an exhibit' do
+        exhibit_1 = Exhibit.create!(name: "Ancient Rome", start_date: "2022-11-15", end_date: "2023-02-14", on_display: true, price: 15.00)
+
+        artifact_1 = Artifact.create!(exhibit: exhibit_1, name: "Statue of Augustus", material: "marble", year_created: "45 BCE", on_loan: false, total_pieces: 5) 
+        artifact_2 = Artifact.create!(exhibit: exhibit_1, name: "Nummus Aureus Coin", material: "gold", year_created: "312 CE", on_loan: true, total_pieces: 12) 
+        artifact_3 = Artifact.create!(exhibit: exhibit_1, name: "Galdiator Cup", material: "glass", year_created: "75 BCE", on_loan: true, total_pieces: 1) 
+
+        expect(exhibit_1.count_of_artifacts).to eq(3)
       end
     end
   end
