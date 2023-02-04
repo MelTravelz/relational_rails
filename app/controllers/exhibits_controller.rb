e37ce0cf2cd2f63bc4ecf9e4ad1f58a275edc861
Melony Franchini <meltravelz@gmail.com>
2023-02-03 17:31:22 -0700

    Refactor create action in exhibits_controller

--- app/controllers/exhibits_controller.rb
+++ app/controllers/exhibits_controller.rb
@@ -15,7 +15,13 @@ class ExhibitsController < ApplicationController
   end
 
   def create
-    exhibit = Exhibit.create!(name: params[:name], on_display: params[:on_display], price: params[:price])
+    # This is one way to do this -> OR create the helper method below and jump to next line.
+    # exhibit = Exhibit.create!(name: params[:name], on_display: params[:on_display], price: params[:price])
+    exhibit = Exhibit.create!(exhibit_params)
     redirect_to "/exhibits"
+  end
+
+  def exhibit_params
+    params.permit(:name, :on_display, :price)
   end
 end
