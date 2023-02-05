a5d993bfa97fd724ff2aaeb27ed6d005f5699880
Melony Franchini <meltravelz@gmail.com>
2023-02-05 16:41:39 -0700

    Add part2 of user story 23/add delete link for artifact

--- spec/features/exhibits/artifacts/index_spec.rb
+++ spec/features/exhibits/artifacts/index_spec.rb
@@ -80,4 +80,44 @@ RSpec.describe 'the exhibit/artifacts index page' do
       end
     end
   end
+
+  describe 'user story 23' do
+    describe 'when I visit "/exhibits/:id/artifacts"' do
+      it 'next to each record, I see a link to delete that artifact record' do
+        exhibit_1 = Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00)
+        artifact_1 = Artifact.create!(exhibit: exhibit_1, name: "Roof-end Tile with Face Design", material: "tile", year_created: "800 BCE", total_pieces: 1, on_loan: true) 
+        artifact_2 = Artifact.create!(exhibit: exhibit_1, name: "Divine Bell of King Seongdeok", material: "metal", year_created: "771 BCE", total_pieces: 2, on_loan: true) 
+
+        visit "/exhibits/#{exhibit_1.id}/artifacts"
+
+        expect(page).to have_link("Delete Artifact: #{artifact_1.name}")
+        expect(page).to have_link("Delete Artifact: #{artifact_2.name}")
+      end
+
+      it 'can delete an artifact & redirected to artifacts index page' do
+        exhibit_1 = Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00)
+        artifact_1 = Artifact.create!(exhibit: exhibit_1, name: "Roof-end Tile with Face Design", material: "tile", year_created: "800 BCE", total_pieces: 1, on_loan: true) 
+        artifact_2 = Artifact.create!(exhibit: exhibit_1, name: "Divine Bell of King Seongdeok", material: "metal", year_created: "771 BCE", total_pieces: 2, on_loan: true) 
+
+        visit "/exhibits/#{exhibit_1.id}/artifacts"
+        click_link("Delete Artifact: #{artifact_1.name}")
+
+        expect(current_path).to eq("/artifacts")   
+
+        expect(page).to_not have_content(artifact_1.name)
+        expect(page).to_not have_content("Material: #{artifact_1.material}")
+        expect(page).to_not have_content("Date Created: #{artifact_1.year_created}")
+        expect(page).to_not have_content("Total Pieces: #{artifact_1.total_pieces}")
+         # Not sure what to do with this test: for now it passes but since its a boolean, most likely
+        # another artifact would have this same output
+        # expect(page).to_not have_content("On Loan from Another Museum: #{artifact_1.on_loan}")
+
+        expect(page).to have_content(artifact_2.name)
+        expect(page).to have_content("Material: #{artifact_2.material}")
+        expect(page).to have_content("Date Created: #{artifact_2.year_created}")
+        expect(page).to have_content("Total Pieces: #{artifact_2.total_pieces}")
+        expect(page).to have_content("On Loan from Another Museum: #{artifact_2.on_loan}")
+      end
+    end
+  end
 end
