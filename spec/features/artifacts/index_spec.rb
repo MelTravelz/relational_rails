a1461e122406ccb6403b92116e522e705c04f417
Melony Franchini <meltravelz@gmail.com>
2023-02-06 21:46:21 -0700

    Update all exhibits index_spec tests

--- spec/features/artifacts/index_spec.rb
+++ spec/features/artifacts/index_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'the artifact index page' do
-  let!(:exhibit_1) { Exhibit.create!(name: "Ancient Rome", on_display: true, price: 15.00) }
-  let!(:artifact_1) { Artifact.create!(exhibit: exhibit_1, name: "Statue of Augustus", material: "marble", year_created: "45 BCE", total_pieces: 5, on_loan: false) }
-  let!(:artifact_2) { Artifact.create!(exhibit: exhibit_1, name: "Galdiator Cup", material: "glass", year_created: "75 BCE", total_pieces: 1, on_loan: true) }  
-  let!(:artifact_3) { Artifact.create!(exhibit: exhibit_1, name: "Nummus Aureus Coin", material: "gold", year_created: "312 CE", total_pieces: 12, on_loan: true) }
-  
-  let!(:exhibit_2) { Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00) }
-  let!(:artifact_4) { Artifact.create!(exhibit: exhibit_2, name: "Roof-end Tile with Face Design", material: "tile", year_created: "800 BCE", total_pieces: 1, on_loan: true) }
-  let!(:artifact_5) { Artifact.create!(exhibit: exhibit_2, name: "Divine Bell of King Seongdeok", material: "metal", year_created: "771 BCE", total_pieces: 2, on_loan: true) }
+  describe 'when I visit "/artifacts"'do 
+    let!(:exhibit_1) { Exhibit.create!(name: "Ancient Rome", on_display: true, price: 15.00) }
+    let!(:artifact_1) { Artifact.create!(exhibit: exhibit_1, name: "Statue of Augustus", material: "marble", year_created: "45 BCE", total_pieces: 5, on_loan: false) }
+    let!(:artifact_2) { Artifact.create!(exhibit: exhibit_1, name: "Galdiator Cup", material: "glass", year_created: "75 BCE", total_pieces: 1, on_loan: true) }  
+    let!(:artifact_3) { Artifact.create!(exhibit: exhibit_1, name: "Nummus Aureus Coin", material: "gold", year_created: "312 CE", total_pieces: 12, on_loan: true) }
+    
+    let!(:exhibit_2) { Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00) }
+    let!(:artifact_4) { Artifact.create!(exhibit: exhibit_2, name: "Roof-end Tile with Face Design", material: "tile", year_created: "800 BCE", total_pieces: 1, on_loan: true) }
+    let!(:artifact_5) { Artifact.create!(exhibit: exhibit_2, name: "Divine Bell of King Seongdeok", material: "metal", year_created: "771 BCE", total_pieces: 2, on_loan: true) }
 
-  describe 'user story 3' do
-    describe 'when I visit "/artifacts"'do 
+    describe 'user story 3' do
       it 'displays all the artifacts and their attributes' do
         visit "/artifacts"
 
@@ -27,82 +27,72 @@ RSpec.describe 'the artifact index page' do
         expect(page).to have_content(artifact_5.name)
       end
     end
-  end
 
-  describe 'user story 8' do
-    describe 'when I visit "/artifacts"'do 
+    describe 'user story 8' do
       it 'I see a link at the top of the page that takes me to the Artifacts Index page' do
         visit "/artifacts"
 
         expect(page).to have_link("All Artifacts", href: "/artifacts")
       end
     end
-  end
 
-  describe 'user story 9' do
-    describe 'when I visit "/artifacts"'do 
+    describe 'user story 9' do
       it 'I see a link at the top of the page that takes me to the Exhibits Index page' do
         visit "/artifacts"
 
         expect(page).to have_link("All Exhibits", href: "/exhibits")
       end
     end
-  end
 
-  describe 'user story 15' do
-    describe 'when I visit "/artifacts"'do 
-      it 'next to each record, I see a link to sort that artifact record' do
-        visit "/artifacts" 
+    describe 'user story 15' do
+        it 'next to each record, I see a link to sort that artifact record' do
+          visit "/artifacts" 
 
-        expect(page).to have_link("Only View Artifact from Other Museums")
-      end 
+          expect(page).to have_link("Only View Artifact from Other Museums")
+        end 
 
-      it 'when I click on the link it displays only the artifacts with a true value for on_loan' do 
-        visit "/artifacts" 
+        it 'when I click on the link it displays only the artifacts with a true value for on_loan' do 
+          visit "/artifacts" 
 
-        expect(page).to have_content(artifact_1.name)
-        expect(page).to have_content(artifact_2.name)
-        expect(page).to have_content(artifact_3.name)
-        expect(page).to have_content(artifact_4.name)
-        expect(page).to have_content(artifact_5.name)
+          expect(page).to have_content(artifact_1.name)
+          expect(page).to have_content(artifact_2.name)
+          expect(page).to have_content(artifact_3.name)
+          expect(page).to have_content(artifact_4.name)
+          expect(page).to have_content(artifact_5.name)
 
-        click_link("Only View Artifact from Other Museums")
+          click_link("Only View Artifact from Other Museums")
 
-        expect(current_path).to eq("/artifacts")  
-        expect(page).to_not have_content(artifact_1.name)
+          expect(current_path).to eq("/artifacts")  
+          expect(page).to_not have_content(artifact_1.name)
 
-        expect(page).to have_content(artifact_2.name)
-        expect(page).to have_content(artifact_3.name)
-        expect(page).to have_content(artifact_4.name)
-        expect(page).to have_content(artifact_5.name)
-      end
+          expect(page).to have_content(artifact_2.name)
+          expect(page).to have_content(artifact_3.name)
+          expect(page).to have_content(artifact_4.name)
+          expect(page).to have_content(artifact_5.name)
+        end
     end
-  end
 
-  describe 'user story 18' do
-    describe 'when I visit "/artifacts"' do
-      it 'next to each record, I see a link to edit that artifact record' do
-        visit "/artifacts"
+    describe 'user story 18' do
+        it 'next to each record, I see a link to edit that artifact record' do
+          visit "/artifacts"
 
-        expect(page).to have_link("Update", href: "/artifacts/#{artifact_1.id}/edit")
-        expect(page).to have_link("Update", href: "/artifacts/#{artifact_2.id}/edit")
-        expect(page).to have_link("Update", href: "/artifacts/#{artifact_3.id}/edit")
-        expect(page).to have_link("Update", href: "/artifacts/#{artifact_4.id}/edit")
-        expect(page).to have_link("Update", href: "/artifacts/#{artifact_5.id}/edit")
-      end
+          expect(page).to have_link("Update", href: "/artifacts/#{artifact_1.id}/edit")
+          expect(page).to have_link("Update", href: "/artifacts/#{artifact_2.id}/edit")
+          expect(page).to have_link("Update", href: "/artifacts/#{artifact_3.id}/edit")
+          expect(page).to have_link("Update", href: "/artifacts/#{artifact_4.id}/edit")
+          expect(page).to have_link("Update", href: "/artifacts/#{artifact_5.id}/edit")
+        end
 
-      it 'when I click on the link it takes me to the artifacts edit page' do 
-        visit "/artifacts"
+        it 'when I click on the link it takes me to the artifacts edit page' do 
+          visit "/artifacts"
 
-        click_link("Update", href: "/artifacts/#{artifact_3.id}/edit")
+          click_link("Update", href: "/artifacts/#{artifact_3.id}/edit")
 
-        expect(current_path).to eq("/artifacts/#{artifact_3.id}/edit")      
-      end
+          expect(current_path).to eq("/artifacts/#{artifact_3.id}/edit")      
+        end
     end
-  end
-  
-  describe 'user story 23' do
-    describe 'when I visit "/artifacts"' do
+    
+    describe 'user story 23' do
       it 'next to each record, I see a link to delete that artifact record' do
         visit "/artifacts"
 
@@ -128,5 +118,4 @@ RSpec.describe 'the artifact index page' do
       end
     end
   end
-
 end
--- spec/features/exhibits/index_spec.rb
+++ spec/features/exhibits/index_spec.rb
@@ -1,64 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe 'the exhibit index page' do
-  describe 'user story 1' do
-    describe 'when I visit "/exhibits"'do 
-      it 'displays all the exhibit names' do
-        exhibit_1 = Exhibit.create!(name: "Ancient Rome", on_display: true, price: 15.00)
-        exhibit_2 = Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00)
+  describe 'when I visit "/exhibits"'do 
+    let!(:exhibit_1) { Exhibit.create!(name: "Ancient Rome", on_display: true, price: 15.00, created_at: Time.now - 2.hour) }
+    let!(:exhibit_2) { Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00, created_at: Time.now - 1.hour) }
 
+    describe 'user story 1' do
+      it 'displays all the exhibit names' do
         visit "/exhibits"
 
         expect(page).to have_content(exhibit_1.name)
         expect(page).to have_content(exhibit_2.name)
       end
     end
-  end
 
-  describe 'user story 6' do
-    describe 'when I visit "/exhibits"'do 
+    describe 'user story 6' do
       it 'displays exhibits ordered by most recently created first' do
-        exhibit_1 = Exhibit.create!(name: "Ancient Rome", on_display: true, price: 15.00, created_at: Time.now - 3.hour)
-        exhibit_2 = Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00, created_at: Time.now - 2.hour)
-
         visit "/exhibits"
 
         expect(exhibit_2.name).to appear_before(exhibit_1.name)
       end
 
       it 'displays when it was created_at next to each of the records' do
-        exhibit_1 = Exhibit.create!(name: "Ancient Rome", on_display: true, price: 15.00, created_at: Time.now - 2.hour)
-        exhibit_2 = Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00, created_at: Time.now - 1.hour)
-
         visit "/exhibits"
 
         expect(page).to have_content("Created at: #{exhibit_1.created_at}")
         expect(page).to have_content("Created at: #{exhibit_2.created_at}")
       end
     end
-  end
 
-  describe 'user story 8' do
-    describe 'when I visit "/exhibits"'do 
+    describe 'user story 8' do
       it 'I see a link at the top of the page that takes me to the Artifacts Index page' do
         visit "/exhibits"
         expect(page).to have_link("All Artifacts", href: "/artifacts")
       end
     end
-  end
 
-  describe 'user story 9' do
-    describe 'when I visit "/exhibits"'do 
+    describe 'user story 9' do
       it 'I see a link at the top of the page that takes me to the Exhibits Index page' do
         visit "/exhibits"
         expect(page).to have_link("All Exhibits", href: "/exhibits")
       end
     end
-  end
 
 
-  describe 'user story 11' do
-    describe 'when I visit "/exhibits"' do
+    describe 'user story 11' do
       it 'I see a link to create a new exhibit record' do
         visit "/exhibits"
         expect(page).to have_link("New Exhibit")
@@ -70,50 +56,34 @@ RSpec.describe 'the exhibit index page' do
         expect(current_path).to eq("/exhibits/new")      
       end
     end
-  end
 
-  describe 'user story 17' do
-    describe 'when I visit "/exhibits"' do
+    describe 'user story 17' do
       it 'next to each record, I see a link to edit that exhibit record' do
-        exhibit_1 = Exhibit.create!(name: "Ancient Rome", on_display: true, price: 15.00)
-        exhibit_2 = Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00)
-        
         visit "/exhibits"
 
-        expect(page).to have_link("Update Exhibit: #{exhibit_1.name}")
-        expect(page).to have_link("Update Exhibit: #{exhibit_2.name}")
+        expect(page).to have_link("Update", href: "/exhibits/#{exhibit_1.id}/edit")
+        expect(page).to have_link("Update", href: "/exhibits/#{exhibit_2.id}/edit")
       end
 
       it 'when I click on the link it takes me to the exhibits edit page' do 
-        exhibit_1 = Exhibit.create!(name: "Ancient Rome", on_display: true, price: 15.00)
-        exhibit_2 = Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00)
-
         visit "/exhibits"
-        click_link("Update Exhibit: #{exhibit_1.name}")
+        click_link("Update", href: "/exhibits/#{exhibit_1.id}/edit")
 
         expect(current_path).to eq("/exhibits/#{exhibit_1.id}/edit")      
       end
     end
-  end
 
-  describe 'user story 22' do
-    describe 'when I visit "/exhibits"' do
+    describe 'user story 22' do
       it 'next to each record, I see a link to delete that exhibit record' do
-        exhibit_1 = Exhibit.create!(name: "Ancient Rome", on_display: true, price: 15.00, created_at: Time.now - 2.hour)
-        exhibit_2 = Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00, created_at: Time.now - 1.hour)
-
         visit "/exhibits"
 
-        expect(page).to have_link("Delete Exhibit: #{exhibit_1.name}")
-        expect(page).to have_link("Delete Exhibit: #{exhibit_2.name}")
+        expect(page).to have_link("Delete", href: "/exhibits/#{exhibit_1.id}")
+        expect(page).to have_link("Delete", href: "/exhibits/#{exhibit_2.id}")
       end
 
       it 'can delete an exhibit(and all associated artifacts) & redirected to exhibit index page' do
-        exhibit_1 = Exhibit.create!(name: "Ancient Rome", on_display: true, price: 15.00, created_at: Time.now - 2.hour)
-        exhibit_2 = Exhibit.create!(name: "Ancient Korea", on_display: false, price: 17.00, created_at: Time.now - 1.hour)
-
         visit "/exhibits"
-        click_link("Delete Exhibit: #{exhibit_1.name}")
+        click_link("Delete", href: "/exhibits/#{exhibit_1.id}")
 
         expect(current_path).to eq("/exhibits")  
 
